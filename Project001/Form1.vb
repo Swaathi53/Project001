202cbca2b94709b5e99ef84e6d1450127a6deb2d
Prashanthi Ramesh <rvprashanthi@gmail.com>
2017-03-13 16:41:03 +0530

    First Commit

---
+++ Project001/Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports ADOX
+Imports System.Data.OleDb
+
+Public Class FormLogin
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+
+End Class
